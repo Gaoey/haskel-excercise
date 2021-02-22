4160a89639e336d55678cc3ef912020adde51746
gaoey <gaoey@hotmail.com>
2021-02-22 13:54:04 +0700

    update polygon

--- polygon/polygon.hs
+++ polygon/polygon.hs
@@ -15,11 +15,26 @@ traversal (x1:x2:xs) = distance x1 x2 + traversal (x2:xs)
 edge :: [(Double, Double)] -> Double
 edge xs = distance (head xs) (last xs)
 
-solve :: [(Double, Double)] -> Double
-solve xs = edge xs + traversal xs
+surface :: [(Double, Double)] -> Double
+surface xs = edge xs + traversal xs
+
+--- see more https://www.mathopenref.com/coordpolygonarea.html
+--- area of irregular polygon
+crossMultiply :: (Double, Double) -> (Double, Double) -> Double
+crossMultiply x1 x2 = (fst x1 * snd x2) - (snd x1 * fst x2)
+
+traversalArea :: [(Double, Double)] -> Double
+traversalArea [x1] = 0
+traversalArea (x1:x2:xs) = crossMultiply x1 x2 + traversalArea (x2:xs)
+
+edgeArea :: [(Double, Double)] -> Double
+edgeArea xs = crossMultiply (last xs) (head xs) 
+
+area :: [(Double, Double)] -> Double
+area xs = abs (edgeArea xs + traversalArea xs) / 2
 
 main = do 
     contents <- readFile("testcase.txt")
     let inputs = map read . words $ contents
-    let result = show . solve . tuplify . map fromIntegral . tail $ inputs
+    let result = show . area . tuplify . map fromIntegral . tail $ inputs
     putStrLn result
--- polygon/readme.md
+++ polygon/readme.md
@@ -1,3 +1,3 @@
-## Compute the Perimeter of a Polygon
+## Compute the Perimeter and Area of a Polygon
 
 https://www.hackerrank.com/challenges/lambda-march-compute-the-perimeter-of-a-polygon/problem
